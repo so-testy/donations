6b059338a83a6ed365665ce79ff4465f394725ca
Ladanov Nikolay <ladanov_n@mail.ru>
2020-09-14 10:36:32 +0300

    Updates

---
+++ precache-manifest.3c5fd53cbee10f70d544071a30c0f49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c9415292905847d592b3e944ea67c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d145d3eab631a51dbbb0",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "d145d3eab631a51dbbb0",
+    "url": "/static/js/2.c856e01a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c856e01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac53d7d458da49ed383",
+    "url": "/static/js/main.a39c68d4.chunk.js"
+  },
+  {
+    "revision": "5dfdcbecd70db25088a2",
+    "url": "/static/js/runtime-main.1891fb70.js"
+  }
+]);
